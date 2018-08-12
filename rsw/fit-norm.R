8d9716f979ae4c8b437b2fd8cc0f4f974bbcf945
Carlisle Rainey <carlislerainey@gmail.com>
2018-08-12 07:26:03 -0400

    Switch norm to full RSW data set

--- rsw/fit-norm.R
+++ rsw/fit-norm.R
@@ -9,32 +9,20 @@ library(bayesplot)
 
 # load simulated data
 rsw_df <- read_csv("rsw/budget.csv") %>%
-  filter(year == 2011) %>%
-  mutate(leg_total_rs = arm::rescale(leg_total),
-         gov_total_rs = arm::rescale(gov_total)) %>%
   glimpse()
 
-# sum_df <- rsw_df %>%
-#   mutate(sign_leg_total = sign(leg_total)) %>%
-#   group_by(year) %>%
-#   summarize(prop_neg = mean(sign_leg_total == -1),
-#             prop_pos = mean(sign_leg_total == 1)) %>%
-#   glimpse()
-
-
 # format data for stan
-f <- leg_total_rs ~ gov_total
+f <- leg_total ~ gov_total
 mf <- model.frame(f, data = rsw_df)
 mm <- model.matrix(f, mf)
-stan_data_list <- list(y = mf$leg_total_rs, 
+stan_data_list <- list(y = mf$leg_total, 
                        X = mm, 
                        N = nrow(mm), 
                        K = ncol(mm))
 
 # simple linear model fit with least squares
 fit_lm <- lm(f, data = rsw_df)
-plot(fit_lm)
-
+arm::display(fit_lm)
 # fit stan model and write to file
 fit <- stan("stan/norm.stan", 
             data = stan_data_list, 
@@ -51,20 +39,28 @@ loo0 <- loo(log_lik0)
 write_rds(loo0, path = "rsw/gen/loo-norm.rds")
 
 # observed data
-obs_df <- data.frame(state_abbr = rsw_df$state_abbr, y = rsw_df$leg_total) %>%
+obs_df <- data.frame(state_abbr = rsw_df$state_abbr, 
+                     year = rsw_df$year, 
+                     x = rsw_df$gov_total,
+                     y = rsw_df$leg_total) %>%
   mutate(state_abbr = reorder(state_abbr, y))
 
 # simulated data from model
 y_rep <- extract(fit, "y_rep")$y_rep
 rep_df <- NULL
-for (i in 1:100) {
-  rep_df_i <- data.frame(state_abbr = rsw_df$state_abbr, y_rep = y_rep[i, ], rep = i)
+for (i in 1:10) {
+  rep_df_i <- data.frame(state_abbr = rsw_df$state_abbr, 
+                         year = rsw_df$year,
+                         x = rsw_df$gov_total,
+                         y_rep = y_rep[i, ], 
+                         rep = i)
   rep_df <- bind_rows(rep_df, rep_df_i)
 }
 glimpse(rep_df)
 
 # posterior predictive distribution by state
 ggplot() + 
-  geom_point(data = rep_df, aes(x = y_rep, y = state_abbr), color = "red", alpha = 0.3) + 
-  geom_point(data = obs_df, aes(x = y, y = state_abbr)) + 
+  geom_point(data = rep_df, aes(x = x, y = y_rep), color = "red", alpha = 0.3) + 
+  geom_point(data = obs_df, aes(x = x, y = y)) + 
+  #facet_wrap(~ year) +
   theme_bw()
